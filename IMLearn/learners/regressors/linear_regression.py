4a071df670925062e9d7eb01bcd912e648c66747
brit_alon1 <brit_alon1@mail.huji.ac.il>
2022-03-29 14:00:18 +0300

    29.3

--- IMLearn/learners/regressors/linear_regression.py
+++ IMLearn/learners/regressors/linear_regression.py
@@ -3,6 +3,7 @@ from typing import NoReturn
 from ...base import BaseEstimator
 import numpy as np
 from numpy.linalg import pinv
+from ...metrics import loss_functions
 
 
 class LinearRegression(BaseEstimator):
@@ -49,7 +50,9 @@ class LinearRegression(BaseEstimator):
         -----
         Fits model with or without an intercept depending on value of `self.include_intercept_`
         """
-        raise NotImplementedError()
+        self.coefs_ = pinv(X.T) @  y
+        if self.include_intercept_:
+            np.insert(self.coefs_, 0, 1)
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -65,7 +68,7 @@ class LinearRegression(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        raise NotImplementedError()
+        return X @ self.coefs_
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -84,4 +87,4 @@ class LinearRegression(BaseEstimator):
         loss : float
             Performance under MSE loss function
         """
-        raise NotImplementedError()
+        return loss_functions.mean_square_error(y, self.predict(X))
--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -7,8 +7,39 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.io as pio
+
 pio.templates.default = "simple_white"
 
+THIS_YEAR = 2022
+
+
+# functions added #
+
+def erase_invalid_rows(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    erase rows with blanks or rows that contains zero in columns that can not be zero (sqft of living room and lot, number of floors, year the house was built in,
+    :param data:
+    :return:
+    """
+    data.dropna(inplace=True)
+    return data[
+        (data.sqft_living > 0) & (data.sqft_lot > 0) & (data.price > 0) & (data.floors > 0) & (data.yr_built > 0) & (
+                data.sqft_living15 > 0) & (data.sqft_lot15 > 0)]
+
+
+def process_data(data: pd.DataFrame) -> pd.DataFrame:
+    data = erase_invalid_rows(data)
+    # modify the yr_built column to express the time passed from the house building.
+    data.loc[:, 'yr_built'] = data['yr_built'].apply(lambda x: THIS_YEAR - x)
+    # modify the yr_renovated column to express the time passed from last renovation.
+    data.loc[:, 'yr_renovated'] = data['yr_renovated'].apply(lambda x: (1 / (THIS_YEAR - x)) if (x > 0) else x)
+    # modify the zipcode data
+    data['zipcode'] = data['zipcode'].astype(int)
+    zipcode_ranges = [i for i in range(data['zipcode'].min(), data['zipcode'].max(), 10)]
+    return data
+
+
+# original exercise functions #
 
 def load_data(filename: str):
     """
@@ -23,7 +54,11 @@ def load_data(filename: str):
     Design matrix and response vector (prices) - either as a single
     DataFrame or a Tuple[DataFrame, Series]
     """
-    raise NotImplementedError()
+    data = pd.read_csv(filename)
+    data = process_data(data)
+    y_true = data['price']
+    data = data.drop('price', axis=1)
+    return data, y_true
 
 
 def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") -> NoReturn:
@@ -49,13 +84,13 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") ->
 if __name__ == '__main__':
     np.random.seed(0)
     # Question 1 - Load and preprocessing of housing prices dataset
-    raise NotImplementedError()
+    load_data('C:\\Users\\brita\\IML.HUJI\\datasets\\house_prices.csv')
 
     # Question 2 - Feature evaluation with respect to response
-    raise NotImplementedError()
+    # raise NotImplementedError()
 
     # Question 3 - Split samples into training- and testing sets.
-    raise NotImplementedError()
+    # raise NotImplementedError()
 
     # Question 4 - Fit model over increasing percentages of the overall training data
     # For every percentage p in 10%, 11%, ..., 100%, repeat the following 10 times:
@@ -64,4 +99,4 @@ if __name__ == '__main__':
     #   3) Test fitted model over test set
     #   4) Store average and variance of loss over test set
     # Then plot average loss as function of training size with error ribbon of size (mean-2*std, mean+2*std)
-    raise NotImplementedError()
+    # raise NotImplementedError()

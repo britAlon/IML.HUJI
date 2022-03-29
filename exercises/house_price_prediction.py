5f06b10c7121390cc953c1a5b8f10ae066663b8e
brit_alon1 <brit_alon1@mail.huji.ac.il>
2022-03-29 21:13:47 +0300

    after geohash

--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -7,6 +7,7 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.io as pio
+__base32 = '0123456789bcdefghjkmnpqrstuvwxyz'
 
 pio.templates.default = "simple_white"
 
@@ -33,11 +34,50 @@ def process_data(data: pd.DataFrame) -> pd.DataFrame:
     data.loc[:, 'yr_built'] = data['yr_built'].apply(lambda x: THIS_YEAR - x)
     # modify the yr_renovated column to express the time passed from last renovation.
     data.loc[:, 'yr_renovated'] = data['yr_renovated'].apply(lambda x: (1 / (THIS_YEAR - x)) if (x > 0) else x)
-    # modify the zipcode data
-    data['zipcode'] = data['zipcode'].astype(int)
-    zipcode_ranges = [i for i in range(data['zipcode'].min(), data['zipcode'].max(), 10)]
+    # modify the longitude and latitude into geohash codes that will be dummy values
+    geo_data = data.apply(lambda x: encode(x.lat, x.long, precision=4), axis=1)
+    dummy = pd.get_dummies(geo_data)
+    data = data.merge(dummy, left_index=True, right_index=True)
+    for column in ['zipcode', 'lat', 'long', 'id']:
+        data = data.drop(column, axis=1)
+    print(data.info())
     return data
 
+
+def encode(latitude, longitude, precision=12):
+    """
+    Encode a position given in float arguments latitude, longitude to
+    a geohash which will have the character count precision.
+    """
+    lat_interval, lon_interval = (-90.0, 90.0), (-180.0, 180.0)
+    geohash = []
+    bits = [ 16, 8, 4, 2, 1 ]
+    bit = 0
+    ch = 0
+    even = True
+    while len(geohash) < precision:
+        if even:
+            mid = (lon_interval[0] + lon_interval[1]) / 2
+            if longitude > mid:
+                ch |= bits[bit]
+                lon_interval = (mid, lon_interval[1])
+            else:
+                lon_interval = (lon_interval[0], mid)
+        else:
+            mid = (lat_interval[0] + lat_interval[1]) / 2
+            if latitude > mid:
+                ch |= bits[bit]
+                lat_interval = (mid, lat_interval[1])
+            else:
+                lat_interval = (lat_interval[0], mid)
+        even = not even
+        if bit < 4:
+            bit += 1
+        else:
+            geohash += __base32[ch]
+            bit = 0
+            ch = 0
+    return ''.join(geohash)
 
 # original exercise functions #
 
